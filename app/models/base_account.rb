b8b410ed5174bfe80d19b7b31f8fea787118ed96
Abdullah Numan <anewman15@hotmail.com>
2022-02-16 19:04:50 +0600

    Generate BaseAccount devise model

---
+++ app/models/base_account.rb
@@ -0,0 +1,6 @@
+class BaseAccount < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ spec/factories/base_accounts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :base_account do
+    
+  end
+end
