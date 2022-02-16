13e8f3d390a2539edda4457eafa99f36d9fa803f
Abdullah Numan <anewman15@hotmail.com>
2022-02-16 19:10:15 +0600

    Raise Runtime error for BaseAccount instantitation

--- app/models/base_account.rb
+++ app/models/base_account.rb
@@ -1,6 +1,12 @@
 class BaseAccount < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+  def initialize(*args)
+    raise 'BaseAccount cannot be instantiated! WTH are you doing dev?' if instance_of(BaseAccount)
+
+    super
+  end
 end
