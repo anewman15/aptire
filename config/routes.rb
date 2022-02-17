b5c172218d29f2aaa3affcd39e93503091f29491
Abdullah Numan <anewman15@hotmail.com>
2022-02-17 18:35:15 +0600

    Configure routes.rb and ApplicationController for devise and json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :user_accounts
+  devise_for :user_accounts,
+    only: [:registrations, :confirmations, :sessions],
+    controllers: {
+      registrations: 'user_accounts/registrations',
+      confirmations: 'user_accounts/confirmations',
+      sessions: 'user_accounts/sessions'
+    },
+    defaults: { format: :json }
+
   devise_for :company_accounts
+    only: [:registrations, :confirmations, :sessions],
+      controllers: {
+        registrations: 'company_accounts/registrations',
+        confirmations: 'company_accounts/confirmations',
+        sessions: 'company_accounts/sessions'
+      },
+      defaults: { format: :json }
 end
