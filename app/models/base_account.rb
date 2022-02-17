c36176aeb3ce06ee9c84f6bba87e0a8783a0c034
Abdullah Numan <anewman15@hotmail.com>
2022-02-18 05:23:44 +0600

    Remove registerable module from BaseAccount

--- app/models/base_account.rb
+++ app/models/base_account.rb
@@ -1,8 +1,7 @@
 class BaseAccount < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :recoverable, :rememberable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :confirmable, :validatable
+  # :registerable, :recoverable, :rememberable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :confirmable, :validatable
 
   def initialize(*args)
     raise 'BaseAccount cannot be instantiated! WTH are you doing dev?' if instance_of?(BaseAccount)
