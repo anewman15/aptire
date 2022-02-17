a9abf92f98270302594bbda3d2f60f18f97f1beb
Abdullah Numan <anewman15@hotmail.com>
2022-02-18 05:26:47 +0600

    Modify changed model names in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :user_accounts,
-    only: [:registrations, :confirmations, :sessions],
+  devise_for :users,
+    only: [:confirmations, :sessions],
     controllers: {
-      registrations: 'user_accounts/registrations',
       confirmations: 'user_accounts/confirmations',
       sessions: 'user_accounts/sessions'
     },
     defaults: { format: :json }
 
-  devise_for :company_accounts
-    only: [:registrations, :confirmations, :sessions],
-      controllers: {
-        registrations: 'company_accounts/registrations',
-        confirmations: 'company_accounts/confirmations',
-        sessions: 'company_accounts/sessions'
-      },
-      defaults: { format: :json }
+  devise_for :companies,
+    only: [:confirmations, :sessions],
+    controllers: {
+      confirmations: 'company_accounts/confirmations',
+      sessions: 'company_accounts/sessions'
+    },
+    defaults: { format: :json }
+
+    resources :user_accounts
 end
