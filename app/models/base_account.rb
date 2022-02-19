88da0c2a3946ca6fd94559a245e63cbc68b10445
Abdullah Numan <anewman15@hotmail.com>
2022-02-19 19:30:26 +0600

    Add jwt_authenticatable devise module to BaseAccount

--- app/models/base_account.rb
+++ app/models/base_account.rb
@@ -1,7 +1,9 @@
 class BaseAccount < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Allowlist
+
   # Include default devise modules. Others available are:
   # :registerable, :recoverable, :rememberable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :confirmable, :validatable
+  devise :database_authenticatable, :confirmable, :validatable,  :jwt_authenticatable, jwt_revocation_strategy: self
 
   def initialize(*args)
     raise 'BaseAccount cannot be instantiated! WTH are you doing dev?' if instance_of?(BaseAccount)
