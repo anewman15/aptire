dcbeaece92f5e519a4615892d4a9ee9449fe0116
Abdullah Numan <anewman15@hotmail.com>
2022-02-19 20:08:15 +0600

    Change namscope for Users in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   devise_for :users,
     only: [:confirmations, :sessions],
     controllers: {
-      confirmations: 'user_accounts/confirmations',
-      sessions: 'user_accounts/sessions'
+      confirmations: 'users/confirmations',
+      sessions: 'users/sessions'
     },
     defaults: { format: :json }
 
   devise_for :companies,
     only: [:confirmations, :sessions],
     controllers: {
-      confirmations: 'company_accounts/confirmations',
-      sessions: 'company_accounts/sessions'
+      confirmations: 'companies/confirmations',
+      sessions: 'companies/sessions'
     },
     defaults: { format: :json }
 
-    resources :user_accounts
+    resources :users
 end
